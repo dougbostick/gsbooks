3f07810920dcb2d655f963502d7d82411a9dbcb2
Gary Smith <garysmith193358@gmail.com>
2022-04-15 00:57:28 +0000

    test gary

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use((req, res, next) => {
   if (path.extname(req.path).length) {
     const err = new Error('Not found')
-    err.status = 404
+    err.status = 404;
     next(err)
   } else {
     next()
