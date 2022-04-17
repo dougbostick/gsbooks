13ef30525d0d37418a24f77e07a95baceaabed07
Doug Bostick <dougbostick@Dougs-MacBook.lan>
2022-04-17 16:06:01 -0400

    started product component, set up dummy product data, /api/products, routes and link for product component, but cannot get routes to recognize product component as an export!!

---
+++ client/components/Products.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class Products extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+    };
+  }
+
+  async componentDidMount() {
+    const response = await axios.get("/api/products");
+    const products = response.data;
+    this.setState({ products });
+  }
+
+  render() {
+    console.log(this.state);
+    const books = this.state.products.map((book) => {
+      <li>{book.name}</li>;
+    });
+    return <div>{books}</div>;
+  }
+}
