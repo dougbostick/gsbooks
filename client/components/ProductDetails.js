8ec54e714b59e53ae891feb227aa2a7ca45cf383
Gary Smith <garysmith193358@gmail.com>
2022-04-21 23:02:20 +0000

    some more set up to get update working

--- client/components/ProductDetails.js
+++ client/components/ProductDetails.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
 import { addCartItem } from "../store/cart-item";
+import {updateProduct} from '../store/products'
+
 
 const ProductDetails = (props) => {
   // console.log("props", props);
-  const { addCartItem } = props;
-  const book = props.products.find(
-    (book) => book.id === parseInt(props.match.params.id)
-  );
-
+  const { addCartItem, book, isAdmin } = props;
   console.log(book);
   if (!book) {
     return null;
   }
+  //isAdmin would go in this return as a "if/else", trying to figure out if we need a form to update the product like jpfp, which I would need to change page to have a constructor, or if there is an easier way. -GS
   return (
     <div>
       <div>Book: {book.name}</div>
@@ -24,15 +23,18 @@ const ProductDetails = (props) => {
 
 //addCartItem(book.id)
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, {match}) => {
   return {
+    isAdmin: state.auth.admin,
     products: state.products,
+    book: state.products.find(book => book.id === parseInt(match.params.id))
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     addCartItem: (bookId) => dispatch(addCartItem(bookId)),
+    update: async(book) => await dispatch(updateProduct(book))
   };
 };
 
