4ecf20e789b1ad8f9e91b0afb8ef6e7d6f097a3c
Stefan <sm9dw@virginia.edu>
2022-04-30 17:35:25 -0400

    working guest cart quantity update

--- client/components/GuestCartItem.js
+++ client/components/GuestCartItem.js
@@ -6,6 +6,11 @@ const GuestCartItem = (props) => {
   // console.log("props", props);
   const guestCart = JSON.parse(window.localStorage.getItem("guest_cart"));
   console.log("gc", guestCart);
+  const inventory = new Array(10);
+  for (let i = 0; i < inventory.length; i++) {
+    inventory[i] = i + 1;
+  }
+
 
   const remove = (productId) => {
     const updatedCart = guestCart.filter((cartItem) => {
@@ -14,6 +19,16 @@ const GuestCartItem = (props) => {
     window.localStorage.setItem("guest_cart", JSON.stringify(updatedCart));
     props.history.push('/cartItem')
   };
+
+  const updateQuantity = (id, num) => {
+    for (let item = 0; item<guestCart.length; item++){
+      if (guestCart[item].product.id == id){
+        guestCart[item].quantity = num;
+        window.localStorage.setItem("guest_cart", JSON.stringify(guestCart));
+        props.history.push('/cartItem');
+      }
+    }
+  }
   
   const gcdisplay = guestCart.map((item) => {
     return (
@@ -21,6 +36,18 @@ const GuestCartItem = (props) => {
         <div> Book: {item.product.name}</div>
         <div> Price: {item.product.price}</div>
         <div> Quantity: {item.quantity}</div>
+        <form>
+          <select onChange ={(ev) => updateQuantity(item.product.id, Number(ev.target.value))}>
+            <option>{item.quantity}</option>
+              {inventory.map((inv) => {
+                return (
+                  <option value={inv} key={inv}> 
+                    {inv}
+                  </option>
+                );
+              })}
+          </select>
+        </form>
         <button onClick={() => remove(item.product.id)}>
           Remove from Cart
         </button>
