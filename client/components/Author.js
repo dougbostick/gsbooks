a982019b607de331a1b8d0de16848e8a85fe8be7
Gary Smith <garysmith193358@gmail.com>
2022-05-12 19:52:41 +0000

    layout for author mui is complete

--- client/components/Author.js
+++ client/components/Author.js
@@ -2,46 +2,102 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import { connect } from "react-redux";
 import Searchbar from './Searchbar'
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { addCartItem } from "../store/cart-item";
+import { addGuestCartItem } from "./AddGuestCartItem";
+
+const useStyles = makeStyles({
+    title: {
+        marginLeft: '1.5rem'
+    },
+    
+  media: {
+    height: 275,
+  },
+  
+  product: {
+     margin: '0 1rem',
+  },
+  
+  info: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between'
+  }
+    
+});
+
+
 
 const Products = (props) => {
+  const classes = useStyles()
   const {author} = useLocation().state
-  console.log("AUTHOR", author)
   const {products, authors } = props
-  console.log("PROPS", props)
   
   const authorBooks = products.filter(book => book.author === author)
   const authorInfo = props.authors.filter( auth => auth.name === author)
+  
   const books = authorBooks.map((book) => {
     return (
-      <div key={book.id}>
-          <Link to={`/products/${book.id}`} >
-            <li >{book.name}</li>
-            <div>Price: {book.price}</div>
-          </Link> 
-      </div>
-    );
+   <Grid item key={book.id} xs={4}>
+            <Card className={classes.product}> 
+                <CardActionArea>
+                    <CardContent>
+                        <CardMedia className={classes.media}> </CardMedia>
+                        
+                        <div className={classes.info}>
+                            <Typography variant='h5'> {book.name} </Typography>
+                            <Typography variant='subtitle2' style={{fontSize: '1.3rem'}}> ${book.price} </Typography>
+                        </div>
+                        
+                         <Typography variant='subtitle2' style={{marginTop: '0.3rem'}}> {book.author} </Typography>
+                
+                    </CardContent>
+                    
+                    <Link to={`/products/${book.id}`} >
+                    <Button style={{marginLeft: '1rem', marginBottom: '1rem', color: 'white', fontWeight: 'bold', backgroundColor: 'grey', padding: '0.5rem'}} 
+                    > View Details </Button>
+                    </Link>
+                </CardActionArea>
+            
+            </Card>
+      </Grid>
+      )
   });
 
-  console.log("AUTHOR INFO", authorInfo)
 
   return (
     <div>
-        <Searchbar/>
-        <div > 
-            <h1>{author}</h1>
+      
+        <Card style={{height: '450px', display: 'flex', width: '50%', margin: 'auto', marginTop: '2rem', marginBottom: '2rem'}}> 
            {
              authorInfo.map( _author => {
                return (
-                 <div className='about-author' key={_author.id}>
-                  <img className='author-img' src={`authorPhotos/${_author.imageUrl}`} />
-                  <p className='author-bio'> {_author.bio} </p>
+               <>
+               <div style={{width: '50%', backgroundColor:'red', background: `url(authorPhotos/${_author.imageUrl})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
+                </div>
+                
+                 <div style={{display:'flex', flexDirection: 'column', width:'50%', padding: '2rem', alignItems: 'center'}}>
+                    <Typography variant='h5'> {_author.name} </Typography>
+                  <Typography variant='subtitle2' style={{marginTop: '0.5rem', lineHeight: '2rem'}}> {_author.bio} </Typography>
                  </div>
+                 </>
                )
              })
            }
-        </div>
-         <h3> Books by {author}: </h3>
+        </Card>
+         <Typography variant="h6" style={{marginLeft: '1rem', marginBottom: '1rem'}}> All Books By {author} </Typography>
+         <Grid container spacing={2}>
          {books}
+         </Grid>
     </div>
   );
 };
